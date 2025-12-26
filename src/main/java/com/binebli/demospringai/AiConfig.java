aa5c775e114a25df453fd54cad821ac4e9abd802
Bilel Nebli <bilelnebli13@gmail.com>
2025-12-26 19:29:02 +0100

    Chapter 5 switching chat memory from JDBC to VectorStore-based implementation

--- src/main/java/com/binebli/demospringai/AiConfig.java
+++ src/main/java/com/binebli/demospringai/AiConfig.java
@@ -3,6 +3,7 @@ package com.binebli.demospringai;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
@@ -19,30 +20,14 @@ import javax.sql.DataSource;
 
 @Configuration
 public class AiConfig {
-  @Autowired
-  JdbcChatMemoryRepository chatMemoryRepository;
-  @Bean
-  public ChatMemory chatMemory() {
-      return MessageWindowChatMemory.builder()
-              .chatMemoryRepository(chatMemoryRepository)
-              .maxMessages(10)
-              .build();
-  }
 
-  @Bean
-  ChatMemoryRepository chatMemoryRepository(DataSource dataSource) {
-    return JdbcChatMemoryRepository.builder()
-            .dialect(new PostgresChatMemoryRepositoryDialect())
-            .dataSource(dataSource)
-            .build();
-  }
 
   @Bean
   ChatClient chatClient(
-      ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+      ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
     return chatClientBuilder
         .defaultAdvisors(
-            MessageChatMemoryAdvisor.builder(chatMemory).build(), // #2
+            VectorStoreChatMemoryAdvisor.builder(vectorStore).build(), // #2
             QuestionAnswerAdvisor.builder(vectorStore)
                 .searchRequest(SearchRequest.builder().build())
                 .build())
